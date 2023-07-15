6cbcaafd3e7133ce1ca44d9a5b4252e9d4d8af76
carlashley <git@macapakr.com>
2023-07-15 16:27:03 +1000

    Package attrs and add mixins

--- src/munkireport/mrlib/__init__.py
+++ src/munkireport/mrlib/__init__.py
@@ -1,8 +1,18 @@
 """Core MunkiReport Class"""
 from pathlib import Path
 
+from .mixins import VersioningMixin, SystemAttrsMixin
 
-class MunkiReport:
+__title__ = "mrlib"
+__summary__ = "munkireport-core package for munkireport"
+__uri__ = "https://github.com/carlashley/munkireport-core"
+__version__ = "1.0.20230715"
+__author__ = "Carl Ashley"
+__license__ = "MIT License"
+__copyright__ = f"2023 {__author__}"
+
+
+class MunkiReport(VersioningMixin, SystemAttrsMixin):
     """The MunkiReport parent class."""
 
     def __init__(self, dry_run: bool = False, tmp_dir: Path = Path("/tmp/mrclient")) -> None:
